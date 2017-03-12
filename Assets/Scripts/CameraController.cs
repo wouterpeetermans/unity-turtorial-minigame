8004ed2e433a1439aaaea11d2dc076c9950e9325
Wouter Peetermans <wouter@wouterpeetermans.com>
2017-03-12 19:18:59 +0100

    project done moved to git

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject Player;
+	private Vector3 offset;
+
+	void Start(){
+		offset = transform.position - Player.transform.position;
+	}
+
+	void LateUpdate(){
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Text CountText;
+	public Text WinText;
+	private int count;
+	public float Speed;
+	private Rigidbody rb;
+
+	void Start (){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		displayCount ();
+		WinText.text = "";
+	}
+
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement*Speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag("Pick Up")) {
+			Destroy (other.gameObject);
+			count++;
+			displayCount ();
+		}
+	}
+
+	void displayCount(){
+		CountText.text = "Count: " + count.ToString ();
+		if (count>=13) {
+			WinText.text = "You win!";
+		}
+	}
+}
